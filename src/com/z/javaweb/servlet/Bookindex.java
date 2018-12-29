fa0a328d69a1265601ea9b6f2a5dd1116a68d19a
ZJT111 <2431271338@qq.com>
2018-12-29 16:54:21 +0800

    Java Resources

--- src/com/z/javaweb/servlet/Bookindex.java
+++ src/com/z/javaweb/servlet/Bookindex.java
@@ -1,6 +1,14 @@
 package com.z.javaweb.servlet;
 
 import java.io.IOException;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,9 +35,30 @@ public class Bookindex extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
+		ArrayList<String> bookNames = new ArrayList<String>();
+		Connection con;
+		try {
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/bookdb", "postgres", "1");
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery("select *from bookInfo");
+			while(rs.next()) {
+				bookNames.add(rs.getString("bookName"));
+			}
+			rs.close();
+			con.close();
+		}catch(Exception e) {
+			bookNames.add(e.toString());
+		}
+		
+		StringBuilder strBuilder = new StringBuilder();
+		Iterator<String> it = bookNames.iterator();
+		while(it.hasNext()) {
+			strBuilder.append(it.next());
+			
+		}
+		response.getWriter().append(strBuilder.toString());
+		}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
